c22ca9bab3cbec03477530cf680e75fb88601487
Mahendra Karyam <karyammahendra@gmail.com>
2025-06-23 13:28:16 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173, // Render sets PORT dynamically
+  }
 })
